996ec9d34522b191ac2b3a296ce8b484fdcea905
Sebastian Pounders <spounders001@student.butte.edu>
2025-06-01 12:24:40 -0700

    fixed

---
+++ dark-web/src/app/layout.js
@@ -0,0 +1,29 @@
+import { Roboto, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Dark-web",
+  description: "Dark-Web ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${robotoMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
